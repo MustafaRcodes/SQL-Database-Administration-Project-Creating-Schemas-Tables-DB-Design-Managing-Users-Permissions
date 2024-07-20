21838487f3dbba7b726f1d02203c416ea280d6d6
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-19 23:14:29 -0400

    Add files via upload

--- Create schema, tables and columns.sql
+++ Create schema, tables and columns.sql
@@ -1,9 +1,11 @@
-CREATE SCHEMA secondschema DEFAULT CHARACTER SET utf8mb4;
+CREATE SCHEMA secondschema DEFAULT CHARACTER SET utf8mb4; -- Creating schema
 
 USE new_schema;
 
+-- Checking columns from table1 under new_schema 
 -- SELECT t.ID FROM table1 t;
 
+-- Creating tables and columns under new_schema
 CREATE TABLE table2 (
     ID BIGINT NOT NULL,
     my_character_field VARCHAR(50), -- will take both text and numbers -- 
@@ -12,3 +14,21 @@ CREATE TABLE table2 (
     PRIMARY KEY (ID)
 );
 
+-- We need to create schema, called 'marketing_project'
+-- Need to add table called 'publisher' with two columns 'publisher_id' (Integer) and 'publisher_name' (up to 65 chars)
+-- adding second table called 'publisher_spend' with columns 'publisher_id' (Integer), 'month'(date) and spend (decimal)
+
+CREATE SCHEMA marketing_project DEFAULT CHARACTER SET utf8mb4;
+USE marketing_project;
+CREATE TABLE publisher (
+    publisher_id INT NOT NULL,
+    publisher_name VARCHAR(65),
+    PRIMARY KEY (publisher_id)
+);
+CREATE TABLE publisher_spend (
+    publisher_spend_id VARCHAR(45) NOT NULL,
+    publisher_id INT NOT NULL,
+    month DATE NOT NULL,
+    spend DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (publisher_spend_id)
+);
