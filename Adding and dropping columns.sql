44a2c0251701bd8c847a71877bdf07d402152ceb
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-21 17:25:51 -0400

    Add files via upload

--- Adding and dropping columns.sql
+++ Adding and dropping columns.sql
@@ -7,3 +7,15 @@ ADD COLUMN purchase_amount DECIMAL(10,2) AFTER ID;
 
 ALTER TABLE table1
 DROP COLUMN purchase_amount; -- drop only when its really needed -- 
+
+SELECT * FROM candy_products;
+
+SELECT * FROM customer_reviews;
+
+SELECT * FROM employees;
+
+ALTER TABLE employees
+DROP COLUMN hourly_wage;
+
+ALTER TABLE employees
+ADD COLUMN avg_customer_rating DECIMAL(10,1) AFTER position;
