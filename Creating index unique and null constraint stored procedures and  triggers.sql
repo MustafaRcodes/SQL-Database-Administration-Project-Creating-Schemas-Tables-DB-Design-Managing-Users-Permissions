7de9390996ae8e6457a2785ba59f610e947d124a
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-23 13:50:12 -0400

    Add files via upload

---
+++ Creating index unique and null constraint stored procedures and  triggers.sql
@@ -0,0 +1,6 @@
+/* USING INDEXES:
+The best way to improve the perfromance of SELECT operations is to create indexes on one or more of the 
+columns that are tested in the query. The index entries act like pointers to the table rows, allowing the 
+query to quickly determine which rows match a condition in the WHERE clause, and retrieve the other column
+values for those rows. All MySQL data type can be indexed.alter
+*/
